5b7093d0f6f984534b41bc675bbdd98b720e2403
kaleab <alebachewkaleab@gmail.com>
2026-02-11 15:46:35 +0300

    Create User

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
